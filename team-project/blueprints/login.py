da6a2f7825b735d95f842ad5323cc41167f82f69
hoyeon <ghdus223@naver.com>
2021-10-12 12:18:31 +0900

    refresh 토큰

--- team-project/blueprints/login.py
+++ team-project/blueprints/login.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
 
-from flask_jwt_extended import create_access_token, get_csrf_token
+from flask_jwt_extended import create_access_token, get_csrf_token, create_refresh_token, jwt_required, get_jwt_identity
 
 from app import db
 from models.user import User
@@ -20,9 +20,21 @@ def index():
         if user :
             user_info = {'user_id': user.user_id, 'name': user.name}
             access_token_cookie = create_access_token(identity=user_info)
+            refresh_token_cookie = create_refresh_token(identity=user_info)
             csrf_access_token = get_csrf_token(access_token_cookie)
             res = jsonify(result='success', user_name=user.name, user_id=user.user_id, csrf_access_token=csrf_access_token)
             res.set_cookie('access_token_cookie', access_token_cookie, httponly=True)
+            res.set_cookie('refresh_token_cookie', refresh_token_cookie, httponly=True)
             return res
         return jsonify({'result':'fail'})
+
+@login.route('/refresh', methods=['GET'])
+@jwt_required(refresh=True)
+def refresh_token():
+    user_info = get_jwt_identity()
+    new_access_token = create_access_token(user_info)
+    csrf_access_token = get_csrf_token(new_access_token)
+    res = jsonify(result='success', csrf_access_token=csrf_access_token)
+    res.set_cookie('access_token_cookie', new_access_token, httponly=True)
+    return res
 
--- team-project/config.py
+++ team-project/config.py
@@ -6,4 +6,5 @@ BASE_DIR = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/team_project'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-expires_access = timedelta(minutes=30)
+expires_access = timedelta(minutes=20)
+expires_refresh = timedelta(days=30)
